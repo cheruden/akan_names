641871f7b0bdb1e1a9eed3ac11cd288f1bb0259c
cheruden <cheruden25@gmail.com>
2021-04-11 14:54:14 +0300

    validate the labels

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 function getAkanName () {
+    let century= document.getElementById("cent").value;
     let yearOfBirth = document.getElementById("year-input").value;
     let monthOfBirth = Number(document.getElementById("month-input").value);
     let dayOfBirth = Number(document.getElementById("day-input").value);
